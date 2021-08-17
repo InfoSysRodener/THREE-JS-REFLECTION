a9fec915ec83bea1157b934bbf5d297191631007
Rodener Dajes <dajesrodener8@gmail.com>
2021-08-18 01:21:50 +0800

    refraction

--- src/main.js
+++ src/main.js
@@ -17,28 +17,27 @@ const textureLoader = new THREE.TextureLoader().load('./texture/digital.PNG');
 	'./texture/1/ny.png',
 	'./texture/1/pz.png',
 	'./texture/1/nz.png',
-])
+]);
 
-const environmentMapInside = new THREE.CubeTextureLoader().load([
-	'./texture/1/nx.png',
-	'./texture/1/px.png',
-	'./texture/1/ny.png',
-	'./texture/1/py.png',
-	'./texture/1/nz.png',
-	'./texture/1/pz.png',
-])
+// const environmentMapInside = new THREE.CubeTextureLoader().load([
+// 	'./texture/1/px.png',
+// 	'./texture/1/nx.png',
+// 	'./texture/1/py.png',
+// 	'./texture/1/ny.png',
+// 	'./texture/1/pz.png',
+// 	'./texture/1/nz.png',
+// ])
+
+
+// environmentMapInside.mapping = THREE.CubeRefractionMapping;
 
 /**
  * Scene
  */
 const canvas = document.querySelector('#canvas');
 const scene = new SceneManager(canvas);
-let conf = { color : '#555555' }; 
 scene.scene.background = environmentMap;
 scene.addOrbitControl();
-
-
-
 
 /**
  * Light
@@ -50,27 +49,62 @@ scene.add(directionalLight);
 const ambiantLight = new THREE.AmbientLight(0xFFFFFF,1);
 scene.add(ambiantLight);
 
-
-const boxGeometry = new THREE.BoxBufferGeometry(4,4,4, 10, 10, 10);
-const boxMaterial = new THREE.MeshPhongMaterial({ 
-	envMap:environmentMapInside,
-	reflectivity: 0.8,
-	color: 0xFFFFFF,
-	opacity: 0.5,
-	side:THREE.BackSide,
-	transparent: true,
-	// envMapIntensity: 5,
-	premultipliedAlpha: true
-});
-
-
 const settings = {
-	segments: 9,
-	radius: {value: 1 },
+	segments: 10,
+	radius: {value: 0.4 },
 	size: {
 	  value: new THREE.Vector3(4,4,4)
 	}
 }
+
+const settings1 = {
+	segments: 10,
+	radius: {value: 0.4 },
+	size: {
+	  value: new THREE.Vector3(5,5,5)
+	}
+}
+
+// Create cube render target
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 512, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter } );
+
+//CubeCamera
+const cubeCamera = new THREE.CubeCamera( 0.1, 5000, cubeRenderTarget );
+scene.add( cubeCamera );	
+cubeCamera.renderTarget.texture.mapping = THREE.CubeRefractionMapping;
+cubeCamera.renderTarget.texture.encoding = THREE.sRGBEncoding;
+
+const boxGeometry = new THREE.BoxBufferGeometry(6,6,6, 10, 10, 10);
+const boxMaterial = new THREE.MeshPhongMaterial({ 
+	envMap:cubeRenderTarget.texture,
+	reflectivity: 0.9,
+	refractionRatio: 0.8,
+	color: 0xFFFFFF,
+	side:THREE.FrontSide,
+	opacity: 1,
+	transparent: true,
+	// premultipliedAlpha: true
+});
+
+
+
+const cube = new THREE.Mesh(boxGeometry, boxMaterial);
+
+
+// const innerCube = cube.clone();
+// innerCube.material = new THREE.MeshPhongMaterial({
+// 	envMap:cubeRenderTarget.texture,
+// 	reflectivity: 0.5,
+// 	refractionRatio: 0.8,
+// 	color: 0xFFFFFF,
+// 	side:THREE.FrontSide,
+// 	opacity:0.8,
+// 	transparent:true,
+// 	// premultipliedAlpha: true
+// })
+
+
+
 
 boxMaterial.onBeforeCompile = shader => {
   
@@ -86,7 +120,6 @@ boxMaterial.onBeforeCompile = shader => {
 	  `#include <begin_vertex>`,
 	  `#include <begin_vertex>
 	  
-  
 	  vec3 signs = sign(position);
 	  vec3 box = boxSize - vec3(radius);
 	  box = vec3(max(0.0, box.x), max(0.0, box.y), max(0.0, box.z));
@@ -100,61 +133,53 @@ boxMaterial.onBeforeCompile = shader => {
 	  vNormal = transformedNormal;
 	  `
 	);
+ 
 };
 
-
-const cube = new THREE.Mesh(boxGeometry, boxMaterial);
-const outsideCube = cube.clone();
-outsideCube.material = new THREE.MeshPhongMaterial( {
-	envMap:environmentMap,
-	color: 0xffffff,
-	reflectivity: 0.5,
-	opacity: 0.25,
-	side: THREE.FrontSide,
-	transparent: true,
-	envMapIntensity: 10,
-	premultipliedAlpha: true
-});;
-
-outsideCube.material.onBeforeCompile = shader => {
+// innerCube.material.onBeforeCompile = shader => {
   
-	shader.uniforms.boxSize = settings.size;
-	shader.uniforms.radius = settings.radius;
+// 	shader.uniforms.boxSize = settings1.size;
+// 	shader.uniforms.radius = settings1.radius;
 	
-	shader.vertexShader = `
-	uniform vec3 boxSize;
-	uniform float radius;
-	` + shader.vertexShader;
+// 	shader.vertexShader = `
+// 	uniform vec3 boxSize;
+// 	uniform float radius;
+// 	` + shader.vertexShader;
 	
-	shader.vertexShader = shader.vertexShader.replace(
-	  `#include <begin_vertex>`,
-	  `#include <begin_vertex>
+// 	shader.vertexShader = shader.vertexShader.replace(
+// 	  `#include <begin_vertex>`,
+// 	  `#include <begin_vertex>
 	  
   
-	  vec3 signs = sign(position);
-	  vec3 box = boxSize - vec3(radius);
-	  box = vec3(max(0.0, box.x), max(0.0, box.y), max(0.0, box.z));
-	  vec3 p = signs * box;
+// 	  vec3 signs = sign(position);
+// 	  vec3 box = boxSize - vec3(radius);
+// 	  box = vec3(max(0.0, box.x), max(0.0, box.y), max(0.0, box.z));
+// 	  vec3 p = signs * box;
   
-	  transformed = signs * box + normalize(position) * radius;
+// 	  transformed = signs * box + normalize(position) * radius;
 	  
-	  // re-compute normals for correct shadows and reflections
-	  objectNormal = all(equal(p, transformed)) ? normal : normalize(position); 
-	  transformedNormal = normalize(normalMatrix * objectNormal);
-	  vNormal = transformedNormal;
-	  `
-	);
-  };
+// 	  // re-compute normals for correct shadows and reflections
+// 	  objectNormal = all(equal(p, transformed)) ? normal : normalize(position); 
+// 	  transformedNormal = normalize(normalMatrix * objectNormal);
+// 	  vNormal = transformedNormal;
+// 	  `
+// 	);
+// };
+
 
 
 const parentCube = new THREE.Group();
-parentCube.add( cube );
-parentCube.add( outsideCube );
+// parentCube.add(innerCube);
+parentCube.add(cube);
 scene.add( parentCube );
+
 
 gui.add(settings.radius, 'value').min(0).max(1).step(0.1).name('radius');
 gui.add(boxMaterial,'reflectivity').min(0.1).max(1).step(0.1).name('inside Reflection');
-gui.add(outsideCube.material,'reflectivity').min(0.1).max(1).step(0.1).name('outside Reflection');
+gui.add(boxMaterial,'refractionRatio').min(-1).max(1).step(0.01).name('Refraction Ratio').onChange( (value) => {
+	boxMaterial.refractionRatio = value;
+});
+// gui.add(outsideCube.material,'reflectivity').min(0.1).max(1).step(0.1).name('outside Reflection');
 gui.add(boxMaterial,'wireframe');
 
 
@@ -163,10 +188,16 @@ const clock = new THREE.Clock();
 const animate = () => {
 	const elapsedTime = clock.getElapsedTime();
 
-	parentCube.rotation.x = elapsedTime * 0.1;
-	parentCube.rotation.y = - elapsedTime * 0.1;
+	// parentCube.rotation.x = elapsedTime * 0.1;
+	// parentCube.rotation.y =  elapsedTime * 0.1;
 
-	
+
+	parentCube.visible = false;
+	cubeCamera.position.copy(cube.position);
+	cubeCamera.update( scene.renderer, scene.scene );
+	parentCube.visible = true;
+
+
 	scene.onUpdate();
 	scene.onUpdateStats();
 	requestAnimationFrame( animate );
